1f94c8f859d41fbbfa1efd5efe6a6aa57541bfe1
LucasLimaAZ <lucasdelimamonteiro@gmail.com>
2019-11-18 16:15:15 -0300

    slider mobile adjust

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Página não encontrada!</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito">
+        <link rel="shortcut icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}"/>
+        <style>
+            .not-found{
+                font-family: 'Nunito';
+                color:#666;
+                width:100%;
+                text-align:center;
+                margin-top:17%;
+                font-size:60px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1 class="not-found">
+            404 | Página não encontrada!<br>
+            <a href="./">
+                <img src="{{ asset('assets/img/logo.png') }}" width="30%" alt="Karina Cristaldo">
+            </a>
+        </h1>
+    </body>
+</html>
